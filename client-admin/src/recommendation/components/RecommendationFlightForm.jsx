61e50dd2b72fe373c580a4e143f31fe75dd217c3
dariusp0p <dariuspop2005@gmail.com>
2026-03-07 22:57:21 +0200

    refactor: moved everything up one dir

--- client-admin/src/recommendation/components/RecommendationFlightForm.jsx
+++ client-admin/src/recommendation/components/RecommendationFlightForm.jsx
@@ -3,7 +3,6 @@ import recommendationContent from '../config/recommendationContent.js'
 export default function RecommendationFlightForm({
   flightId,
   onChangeFlightId,
-  pastFlightIds,
   onSubmit,
 }) {
   return (
@@ -14,13 +13,7 @@ export default function RecommendationFlightForm({
         onChange={(event) => onChangeFlightId(event.target.value)}
         placeholder={recommendationContent.flightIdPlaceholder}
         className="configurator-modal__input"
-        list="recommendation-flight-id-history"
       />
-      <datalist id="recommendation-flight-id-history">
-        {pastFlightIds.map((id) => (
-          <option key={id} value={id} />
-        ))}
-      </datalist>
       <button type="submit" className="configurator-action-link">
         {recommendationContent.submitLabel}
       </button>
